405a2d642fd3818fea2df6c55e9da7a4d1ab96f2
Ahantya <115478950+Ahantya@users.noreply.github.com>
2024-02-19 01:36:31 -0600

    Create YoutubeDataScraper.py

---
+++ Youtube/YoutubeDataScraper.py
@@ -0,0 +1,45 @@
+from googleapiclient.discovery import build
+import json
+
+apikey = '' # create your own!
+
+
+def fetchdetails(videoid):
+    youtube = build('youtube', 'v3', developerKey=apikey)
+
+    request = youtube.videos().list(
+        part="snippet,statistics",
+        id=videoid
+    )
+
+    response = request.execute()
+
+    videoinfo = {}
+    
+    if 'items' in response and len(response['items']) > 0:
+        
+        item = response['items'][0]
+        videoinfo['title'] = item['snippet']['title']
+        videoinfo['channeltitle'] = item['snippet']['channelTitle']
+        videoinfo['views'] = item['statistics']['viewCount']
+        videoinfo['likes'] = item['statistics'].get('likeCount', 0)
+        # no point in getting dislikes :
+        videoinfo['comments'] = item['statistics'].get('commentCount', 0)
+
+    return videoinfo
+
+if __name__ == "__main__":
+    
+    videoid = input("Enter YouTube video ID: ")
+    video = fetchdetails(videoid)
+
+    if video:
+        print("\nVideo Details:")
+        print("Title:", video['title'])
+        print("Channel Title:", video['channeltitle'])
+        print("Views:", video['views'])
+        print("Likes:", video['likes'])
+        print("Comments:", video['comments'])
+        
+    else:
+        print("Video not found or invalid video ID.")
